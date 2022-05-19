47d4908a5c1430721c76cf7235e5329f0c79a88c
WrightOa <105869110+WrightOa@users.noreply.github.com>
2022-05-19 08:39:00 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title('El ayudante de la pala')
+option = st.selectbox(
+     '¿ Que estas buscando ?',
+     ('Insecticidas', 'Fertilizante', 'Desinfectante RBM-Q','',''))
+st.sidebar.title('¿ Quienes somos ?')
+st.sidebar.write('Somos una empresa socialmente responsable que busca la eficacia y seguridad de productos quimicos en materia de agricultura por medio de nuestra pagina web, garantizando productos de alta calidad y seguridad en transporte. ')
+st.sidebar.image('https://conceptoabc.com/wp-content/uploads/2021/07/Agricultura.jpg')
